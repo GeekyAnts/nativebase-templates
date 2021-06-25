7293050994b3b2e111a3b7473a6dc056fd2be976
Nishan Bende <nishanbende@gmail.com>
2021-06-25 19:03:26 +0530

    cra typescript template

---
+++ cra-template-nativebase-typescript/README.md
@@ -0,0 +1,15 @@
+# cra-template-nativebase
+
+The official NativeBase template for [Create React App](https://github.com/facebook/create-react-app)
+
+## Usage
+
+```sh
+npx create-react-app my-app --template nativebase
+```
+
+## TypeScript
+
+```sh
+npx create-react-app my-app --template nativebase-typescript
+```
---
+++ cra-template-nativebase-typescript/template/src/App.tsx
@@ -0,0 +1,64 @@
+import logo from "./logo.svg";
+import {
+  Box,
+  Image,
+  Text,
+  Link,
+  HStack,
+  Heading,
+  Switch,
+  useColorMode,
+  VStack,
+  Code,
+} from "native-base";
+
+function App() {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Box
+      bg={colorMode === "light" ? "coolGray.50" : "coolGray.900"}
+      minHeight="100vh"
+      justifyContent="center"
+      px={4}
+    >
+      <VStack space={5} alignItems="center">
+        <Image
+          source={{ uri: logo }}
+          resizeMode="contain"
+          size={220}
+          alt="NativeBase logo"
+        />
+        <Heading size="lg">Welcome to NativeBase</Heading>
+        <Text>
+          Edit <Code>src/App.js</Code> and save to reload.
+        </Text>
+        <Link href="https://docs.nativebase.io" isExternal>
+          <Text color="primary.500" underline fontSize={"xl"}>
+            Learn NativeBase
+          </Text>
+        </Link>
+        <ToggleDarkMode />
+      </VStack>
+    </Box>
+  );
+}
+
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack space={2}>
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === "light" ? true : false}
+        onToggle={toggleColorMode}
+        accessibilityLabel={
+          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+        }
+      />
+      <Text>Light</Text>
+    </HStack>
+  );
+}
+
+export default App;
