a2cebc7c0c9bc9af791bb06defa247a07f4c29ce
cyrus25 <cyruspassi@gmail.com>
2021-07-05 11:04:37 +0530

    expo nativebase template

---
+++ expo-nativebase-typescript/App.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  Box,
+  Image,
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Code,
+} from "native-base";
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Center
+        _dark={{ bg: "blueGray.900" }}
+        _light={{ bg: "blueGray.50" }}
+        px={4}
+        flex={1}
+      >
+        <VStack space={5} alignItems="center">
+          <Image
+            source={require("./assets/nativebase-logo.svg")}
+            resizeMode="contain"
+            size={220}
+            alt="NativeBase logo"
+          />
+          <Heading size="lg">Welcome to NativeBase</Heading>
+          <Text>
+            Edit <Code>src/App.js</Code> and save to reload
+          </Text>
+          <Link href="https://docs.nativebase.io" isExternal>
+            <Text color="primary.500" underline fontSize={"xl"}>
+              Learn NativeBase
+            </Text>
+          </Link>
+          <ToggleDarkMode />
+        </VStack>
+      </Center>
+    </NativeBaseProvider>
+  );
+}
+
+// Color Switch Component
+function ToggleDarkMode() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack space={2}>
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === "light" ? true : false}
+        onToggle={toggleColorMode}
+        aria-label={
+          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+        }
+      />
+      <Text>Light</Text>
+    </HStack>
+  );
+}
