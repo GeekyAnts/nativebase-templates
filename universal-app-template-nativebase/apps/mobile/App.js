1ec9ba4d15fc7cdef8ea09c2c251c2be303a98f2
Nishan Bende <nishanbende@gmail.com>
2021-08-26 20:22:33 +0530

    feat: add react navigation

--- universal-app-template-nativebase/apps/mobile/App.js
+++ universal-app-template-nativebase/apps/mobile/App.js
@@ -1,6 +1,9 @@
-import React from "react";
-import { Center, NativeBaseProvider, VStack } from "native-base";
+// In App.js in a new project
 
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Center, NativeBaseProvider, VStack } from "native-base";
 import {
   Greeting,
   LearnNativeBase,
@@ -9,22 +12,36 @@ import {
   ToggleDarkMode,
 } from "universal-components";
 
-export default function App() {
+function HomeScreen() {
+  return (
+    <Center
+      _dark={{ bg: "blueGray.900" }}
+      _light={{ bg: "blueGray.50" }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <NativeBaseIcon />
+        <Greeting filePath="apps/mobile/App.js" />
+        <LearnNativeBase />
+        <ToggleDarkMode />
+      </VStack>
+    </Center>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
     <NativeBaseProvider theme={theme}>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Greeting filePath="apps/mobile/App.js" />
-          <LearnNativeBase />
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
+
+export default App;
