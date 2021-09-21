dceb2c0fe4ba1f7e87c9f51b04e2df5e678ea5f9
cyrus25 <cyruspassi@gmail.com>
2021-09-21 18:00:39 +0530

    updated version for universal templates

--- universal-app-template-nativebase/apps/components/src/components/Greeting.js
+++ universal-app-template-nativebase/apps/components/src/components/Greeting.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Code, Heading, Text } from "native-base";
+import { Code, Heading, HStack, Text } from "native-base";
 
 export function Greeting({ filePath }) {
   return (
     <>
       <Heading size="lg">Welcome to NativeBase</Heading>
-      <Text>
-        Edit <Code>{filePath}</Code> and save to reload.
-      </Text>
+      <HStack space={2} alignItems="center">
+        <Text>Edit</Text>
+        <Code>{filePath}</Code>
+        <Text>and save to reload.</Text>
+      </HStack>
     </>
   );
 }
