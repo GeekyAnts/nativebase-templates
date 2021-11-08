047448c901224e3274e8ddc6440ac4f06a3b423b
cyrus25 <cyruspassi@gmail.com>
2021-11-08 16:58:41 +0530

    cool

--- bump-version.js
+++ bump-version.js
@@ -1,19 +1,37 @@
 
 const path = require('path');
+const { spawn } = require('child_process');
 const { templates, bumpVersion } = require('./utils');
 
 
 init();
 
 
-function init() {
+async function init() {
 
-    templates.forEach(template => {
+    for (let i = 0; i < templates.length; i++) {
 
-        const { name, publishable } = template;
+        const { name, publishable } = templates[i];
         const repoPath = path.resolve(name);
         if (publishable) {
-            bumpVersion(repoPath, name);
+            await new Promise((resolve, reject) => {
+                const child = spawn(`cd ${repoPath} && yarn version --patch`, {
+                    shell: true
+                });
+                child.stdout.on("data", function (data) {
+                    console.log(`${data}`);
+                });
+                child.stderr.on("data", data => {
+                    console.log(`stderr: ${data} `);
+                });
+                child.on("error", error => {
+                    console.log(`error: ${error.message}`);
+                });
+                child.on("close", () => {
+                    console.log('\x1b[32m%s\x1b[0m',);
+                    resolve();
+                });
+            })
         }
-    });
+    };
 }
