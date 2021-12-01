d13d90ce1102ca4361aa242d3a823490ac4ac626
Aditya Jamuar <adityaj@geekyants.com>
2021-12-01 19:08:08 +0530

    next 12 example with typescript working

--- nextjs-with-native-base-typescript/babel.config.js
+++ nextjs-with-native-base-typescript/babel.config.js
@@ -1,4 +1,10 @@
+// @generated: @expo/next-adapter@3.1.10
+// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
+
 module.exports = {
-  presets: ['next/babel'],
-  plugins: [['react-native-web', { commonjs: true }]],
-}
+  presets: ["@expo/next-adapter/babel"],
+  plugins: [
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+  ],
+};
