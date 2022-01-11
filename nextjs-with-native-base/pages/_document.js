a7f63c3bdc0a4aa94d319e5bacfa2e330280b1f1
Vidhi Kataria <vidhikataria@Vidhi-MacBook-Pro.local>
2022-01-11 19:19:20 +0530

    document js updated

--- nextjs-with-native-base/pages/_document.js
+++ nextjs-with-native-base/pages/_document.js
@@ -1,2 +1,39 @@
 // @generated: @expo/next-adapter@3.1.10
-export { default } from '@expo/next-adapter/document';
+import { Children } from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { AppRegistry } from "react-native";
+import config from "../app.json";
+// Force Next-generated DOM elements to fill their parent's height
+const normalizeNextElements = `
+  #__next {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+`;
+
+export default class MyDocument extends Document {
+  static async getInitialProps({ renderPage }) {
+    AppRegistry.registerComponent(config.name, () => Main);
+    const { getStyleElement } = AppRegistry.getApplication(config.name);
+    const page = await renderPage();
+    const styles = [
+      // eslint-disable-next-line react/jsx-key
+      <style dangerouslySetInnerHTML={{ __html: normalizeNextElements }} />,
+      getStyleElement(),
+    ];
+    return { ...page, styles: Children.toArray(styles) };
+  }
+
+  render() {
+    return (
+      <Html style={{ height: "100%" }}>
+        <Head />
+        <body style={{ height: "100%", overflow: "hidden" }}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
