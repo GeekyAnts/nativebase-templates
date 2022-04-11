124067a1912b086d0fa21c691b8934c6c296654e
mayank-96 <pagarmayank07@gmail.com>
2022-04-11 17:02:32 +0530

    feat: version upgrade for rn nativebase typescript

--- react-native-template-nativebase-typescript/template/App.tsx
+++ react-native-template-nativebase-typescript/template/App.tsx
@@ -19,7 +19,7 @@ import {
   useColorMode,
   NativeBaseProvider,
   VStack,
-  Code,
+  Box,
 } from 'native-base';
 import NativeBaseIcon from './src/components/NativeBaseIcon';
 
@@ -53,7 +53,14 @@ const App = () => {
           <Heading size="lg">Welcome to NativeBase</Heading>
           <HStack space={2} alignItems="center">
             <Text>Edit</Text>
-            <Code>App.tsx</Code>
+            <Box
+              px={2}
+              py={1}
+              _dark={{ bg: "blueGray.800" }}
+              _light={{ bg: "blueGray.200" }}
+            >
+              App.js
+            </Box>
             <Text>and save to reload.</Text>
           </HStack>
           <Link href="https://docs.nativebase.io" isExternal>
